59324cb823684ec62e670da606258e25a8584fbe
leandroJader <ldandro>
2025-03-19 14:29:45 -0300

    Add project files.

---
+++ JogoDoDado.consoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace JogoDoDado.consoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("==============");
+                Console.WriteLine("Jogo De Dados");           
+                Console.WriteLine("==============");
+
+                Console.WriteLine("voce deseja sair do jogo s/n");
+                string continuar =(Console.ReadLine())!.ToUpper();
+
+                if (continuar != "S")
+                {
+                    break;
+                }
+                    }
+
+        }
+    }
+}
