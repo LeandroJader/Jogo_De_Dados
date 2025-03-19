2245c72376ea40b2a1c5ed87693eac92e375c607
leandroJader <ldandro>
2025-03-19 14:39:40 -0300

    implementada logica de lançamento de dados

--- JogoDoDado.consoleApp/Program.cs
+++ JogoDoDado.consoleApp/Program.cs
@@ -10,9 +10,17 @@
                 Console.WriteLine("Jogo De Dados");           
                 Console.WriteLine("==============");
 
+                Console.WriteLine("aperte ENTER para jogar o dado ");
+                Console.ReadLine();
+
+                Random gerador_de_numeros = new Random();
+                
+                int Numero_gerado = gerador_de_numeros.Next(1, 7);
+
+                Console.WriteLine($" O numero sorteado foi {Numero_gerado}");
+                
                 Console.WriteLine("voce deseja sair do jogo s/n");
                 string continuar =(Console.ReadLine())!.ToUpper();
-
                 if (continuar != "S")
                 {
                     break;
