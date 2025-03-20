7d20a380b0975a6df9f2d06a9b89762ce4c1e743
leandroJader <ldandro>
2025-03-20 08:44:18 -0300

    implementado eventos especiais de avanço de posição

--- JogoDoDado.consoleApp/Program.cs
+++ JogoDoDado.consoleApp/Program.cs
@@ -4,29 +4,88 @@
     {
         static void Main(string[] args)
         {
+            const int limiteLinhaDeChegada = 30;
+
+
             while (true)
             {
-                Console.WriteLine("==============");
-                Console.WriteLine("Jogo De Dados");           
-                Console.WriteLine("==============");
+             int posicaoJogador = 0;
+             bool jogoEmAndamento = true;
 
-                Console.WriteLine("aperte ENTER para jogar o dado ");
-                Console.ReadLine();
+                while (jogoEmAndamento){
 
-                Random gerador_de_numeros = new Random();
-                
-                int Numero_gerado = gerador_de_numeros.Next(1, 7);
 
-                Console.WriteLine($" O numero sorteado foi {Numero_gerado}");
-                
-                Console.WriteLine("voce deseja sair do jogo s/n");
-                string continuar =(Console.ReadLine())!.ToUpper();
-                if (continuar != "S")
-                {
-                    break;
-                }
+
+
+                    Console.Clear();
+                    Console.WriteLine("==============");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Jogo De Dados");
+                    Console.ResetColor();
+                    Console.WriteLine("===========================");
+
+                    Console.WriteLine("aperte ENTER para jogar o dado ");
+                    Console.ReadLine();
+
+                    Random gerador_de_numeros = new Random();
+
+                    int Numero_gerado = gerador_de_numeros.Next(1, 7);
+
+                    Console.WriteLine("==========================");
+                    Console.WriteLine($"O numero sorteado foi {Numero_gerado}");
+
+                    posicaoJogador += Numero_gerado;
+
+
+                    Console.WriteLine($"voce esta na posicao {posicaoJogador} de {limiteLinhaDeChegada}");
+                   
+                        if (posicaoJogador==5 || posicaoJogador == 10|| posicaoJogador==15 || posicaoJogador==25)
+                    {
+                        Console.WriteLine("=======================");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("EVENTO ESÉCIAL: avanço extra de 3 casas ");
+                        Console.ResetColor();
+
+                        posicaoJogador += 3;
+                        Console.WriteLine($"você avançou para posição {posicaoJogador}");
+                    }
+                    else if (posicaoJogador==7 || posicaoJogador==13 || posicaoJogador == 20)
+                    {
+                        Console.WriteLine("=======================");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("EVENTO ESÉCIAL: recuo de 2 casas ");
+                        Console.ResetColor();
+
+                        posicaoJogador -= 2;
+                        Console.WriteLine($"você recuou para posição {posicaoJogador}");
+
                     }
 
+
+
+                    if (posicaoJogador >= limiteLinhaDeChegada)
+                    {
+                        jogoEmAndamento = false;
+                        Console.WriteLine("=================");
+                        Console.WriteLine("parabens voce chegou a linha de chegada ");
+                        Console.WriteLine("==================");
+                    }
+  
+                    
+                      
+                        Console.ReadLine();
+                    
+
+                }
+                        Console.WriteLine("você deseja continuar jogando s/n");
+                    string continuar = (Console.ReadLine())!.ToUpper();
+                    if (continuar != "S")
+                    {
+                        break;
+                    }
+
+                }
+            
         }
     }
 }
